13d22e7f3df914d13dff9966fecab38bf0945671
Russell Hart <russellbot@gmail.com>
2020-03-18 17:14:01 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,2 +1,22 @@
-var list = ["tiger", "cat", "bear", "bird"];
+var user = {
+	name: "John",
+	age: 34,
+	hobby: "Soccer",
+	isMarried: false,
+	spells: ["abrakadabra", "shazam", "boo"],
+	shout: function() {
+		console.log("AHHHHHH!");
+	}
+};
 
+var list = [
+	{
+		username: "andy",
+		password: "secre"
+	},
+	{
+		username: "jess",
+		password: "123"
+	}
+
+];
